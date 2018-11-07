89e6fa6c7c478472aa9b803a2941cb520ec3828f
jacobkrantz <jkrantz@zagmail.gonzaga.edu>
2018-11-07 12:56:36 -0800

    Move code over from previous repo

---
+++ WebPhapp/backend/spec.js
@@ -0,0 +1,43 @@
+// https://glebbahmutov.com/blog/how-to-correctly-unit-test-express-server/
+var request = require('supertest');
+
+describe('loading express', function () {
+  var server;
+
+  // setup function to reset the server before each test case
+  beforeEach(function () {
+    delete require.cache[require.resolve('./server')];
+    server = require('./server');
+  });
+
+  /*
+   * teardown function to close the server after each test case.
+   * must past mocha 'done' or server does not close.
+   */
+  afterEach(function (done) {
+    server.close(done);
+  });
+
+  // Specify all server test functions below
+
+  it('responds with a list to /api/getList', function testSlash(done) {
+  request(server)
+    .get('/api/getList')
+    .expect(didGetThreeList)
+    .end(done);
+  });
+
+  /* // example for future (currently returns 404 on travis for some reason):
+  it('200 everything else for now', function testPath(done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(200, done);
+  });
+  */
+});
+
+// res: Response
+function didGetThreeList(res){
+    var lst = res.body;
+    if (lst.length !== 3) throw new Error("Body did not contain list of three elements");
+};
