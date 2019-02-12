296275c55d577d444c03fdb927e506069b6763d7
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2019-02-11 22:39:58 -0800

    Add header

--- WebPhapp/client/src/App/pages/App.js
+++ WebPhapp/client/src/App/pages/App.js
@@ -6,11 +6,13 @@ import Home from "./Home";
 import Patient from "./Patient";
 import PatientSearch from "./PatientSearch"
 import PrescriptionAdd from "./PrescriptionAdd.js"
+import Header from "../components/Header.js"
 
 class App extends Component {
   render() {
     const App = () => (
       <div>
+        <Header/>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/patient" component={Patient} />
